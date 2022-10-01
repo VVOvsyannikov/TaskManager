fc9315cbcbc0abac5966ba117c84223b2b9323bf
VVOvsyannikov <vvovsyannikov2015@gmail.com>
2022-10-01 22:41:53 +0300

    fix type error in tests

--- test/factories/sequence.rb
+++ test/factories/sequence.rb
@@ -1,9 +1,11 @@
 FactoryBot.define do
-  sequence :string, aliases: [:first_name, :last_name, :password, :name, :description, :state, :avatar, :type, :expired_at] do |n|
+  sequence :string, aliases: [:first_name, :last_name, :password, :name, :description, :state, :avatar, :expired_at] do |n|
     "string#{n}"
   end
 
   sequence :email do |n|
     "person#{n}@example.com"
   end
+
+  sequence :type, [:Developer, :Manager, :Admin].sample
 end
