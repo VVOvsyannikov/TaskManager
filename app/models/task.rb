6b52f438f08efca8acec53e240499dbd0f4ea051
VVOvsyannikov <vvovsyannikov2015@gmail.com>
2022-10-06 23:22:16 +0300

    delete comment row from task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,4 @@
 class Task < ApplicationRecord
-  # StateMachines::Machine.ignore_method_conflicts = true
-
   belongs_to :author, class_name: 'User'
   belongs_to :assignee, class_name: 'User', optional: true
 
