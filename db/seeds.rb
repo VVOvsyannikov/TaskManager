b9620b9a1ff82108af63fbd4c73f53e37d77861b
VVOvsyannikov <vvovsyannikov2015@gmail.com>
2022-10-30 21:55:59 +0300

    seed db with Tasks

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,18 @@ admin = Admin.find_or_create_by(first_name: 'admin', last_name: 'admin', email: 
 admin.password = 'admin'
 admin.save
 
-60.times do |i|
+10.times do |i|
   u = [Manager, Developer].sample.new
   u.email = "email#{i}@mail.gen"
   u.first_name = "FN#{i}"
   u.last_name = "LN#{i}"
   u.password = "#{i}"
   u.save
+end
+
+states = ['new_task', 'in_development', 'in_qa', 'in_code_review', 'ready_for_release', 'released', 'archived']
+states.each do |state|
+  12.times do |i|
+    Task.create!(name: "Test task #{i}", description: "This is a test task #{i} with state #{state}", author_id: 1, assignee_id: 7, state: state )
+  end
 end
