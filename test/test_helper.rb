b89dc3ee2b90f1087b43be612ca2d75d775ac72d
VVOvsyannikov <vvovsyannikov2015@gmail.com>
2022-11-22 21:41:30 +0300

    fix test_helper: run tests in COVERAGE ENV

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,17 +1,19 @@
-require 'simplecov'
-require 'simplecov-lcov'
+if ENV['COVERAGE']
+  require 'simplecov'
+  require 'simplecov-lcov'
 
-SimpleCov::Formatter::LcovFormatter.config do |c|
-  c.report_with_single_file = true
-  c.single_report_path = './coverage/lcov.info'
+  SimpleCov::Formatter::LcovFormatter.config do |c|
+    c.report_with_single_file = true
+    c.single_report_path = './coverage/lcov.info'
+  end
+  SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new(
+    [
+      SimpleCov::Formatter::HTMLFormatter,
+      SimpleCov::Formatter::LcovFormatter
+    ]
+  )
+  SimpleCov.start
 end
-SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new(
-  [
-    SimpleCov::Formatter::HTMLFormatter,
-    SimpleCov::Formatter::LcovFormatter
-  ]
-)
-SimpleCov.start
 
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
