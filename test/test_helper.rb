f9c74d59e7684cc057a6fac6712d13f15429d31d
Vladimir Ovsyannikov <vvovsyannikov2015@gmail.com>
2025-11-10 18:23:22 +0300

    add coverrals (#20)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,9 +3,6 @@ require_relative '../config/environment'
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  parallelize(workers: :number_of_processors)
   include FactoryBot::Syntax::Methods
   include AuthHelper
-
-  fixtures :all
 end
