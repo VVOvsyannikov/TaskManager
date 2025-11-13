c22b476860547cfe5704241a92232f3fb8999694
VVOvsyannikov <vvovsyannikov2015@gmail.com>
2025-11-13 09:06:20 +0300

    refactored

---
+++ config/initializers/simplecov.rb
@@ -0,0 +1,20 @@
+if ENV['COVERAGE']
+  require 'simplecov'
+
+  SimpleCov.start(:rails) do
+    enable_coverage :branch
+
+    if ENV['COV_IN_DEV_MODE']
+      formatter SimpleCov::Formatter::HTMLFormatter
+    else
+      require 'simplecov-lcov'
+
+      SimpleCov::Formatter::LcovFormatter.config do |config|
+        config.report_with_single_file = true
+        config.single_report_path = 'coverage/lcov.info'
+      end
+
+      formatter SimpleCov::Formatter::LcovFormatter
+    end
+  end
+end
