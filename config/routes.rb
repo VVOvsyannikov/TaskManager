733dce6c2bae44edd2462676372326dfa8b20a28
VVOvsyannikov <vvovsyannikov2015@gmail.com>
2025-11-18 00:33:21 +0300

    refactored controllers

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,14 @@ Rails.application.routes.draw do
   mount Sidekiq::Web => '/admin/sidekiq'
 
   root :to => "web/boards#show"
-  get '/up',                   to: proc { [200, {}, ['OK']] }
+  get '/up', to: proc { [200, {}, ['OK']] }
 
   resource :password_resets, only: [:new, :create, :edit] do
     patch :update, on: :collection
   end
 
-  put 'attach_image',          to: 'images#attach_image'
-  put 'remove_image',          to: 'images#remove_image'
+  # put 'attach_image',          to: 'images#attach_image'
+  # put 'remove_image',          to: 'images#remove_image'
 
   scope module: :web do
     resource :board, only: :show
@@ -24,7 +24,12 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :tasks, only: [:index, :show, :create, :update, :destroy]
+      resources :tasks, only: [:index, :show, :create, :update, :destroy] do
+        member do
+          put :attach_image
+          put :remove_image
+        end
+      end
       resources :users, only: [:index, :show]
     end
   end
